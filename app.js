6d20fe491adeca7d453fef0364b468e9c607b5b4
Rasha <r.alamleh@gmail.com>
2020-11-02 19:10:53 +0200

    R01

--- app.js
+++ app.js
@@ -4,28 +4,32 @@ var PX = false;
 var result = [0,0,0,0,0,0,0,0,0];
 
 function move(button) {
+
     if (!PO && !PX) {
         PO = true;
-    }
+    };
+
     if(PO) {
         button.value = "O";
-        result[parseInt("button.id") - 1] = "O";
+        result[parseInt(button.id) - 1] = "O";
         PO = false;
         PX = true;
         button.disabled = true;
-        }
-    if(PX) {
+        };
+
+     if(PX) {
             button.value = "X";
-            result[parseInt("button.id") - 1] = "X";
+            result[parseInt(button.id) - 1] = "X";
             PO = true;
             PX = false;
             button.disabled = true;
-            }
-        }
+            };
+        };
+
 function won(result) {
     if(result[0] === result[1] === result[2] || result[0] === result[3] === result[6] || result[0] === result[4] === result[8] ) {
         alert("Player " + result[0] + " is the winner")
-    }
+    };
 }
 
        
